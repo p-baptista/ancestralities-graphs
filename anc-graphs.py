193f0635f0201f622bf423556cb0ff8e3d53d6a4
p-baptista <phfb.br@gmail.com>
2023-12-06 12:51:30 -0300

    New config for correct priori-posteriori plotting

--- anc-graphs.py
+++ anc-graphs.py
@@ -16,6 +16,8 @@ NUMBER_PULSES = int(config.get('Basic Configuration', 'N_PULSES'))
 NUMBER_ANCESTRY = int(config.get('Basic Configuration', 'N_ANCESTRIES'))
 ANCESTRY_NAMES = config.get('Basic Configuration', 'ANCESTRIES')
 ANCESTRY_NAMES = ANCESTRY_NAMES.split(',')
+ANCESTRY_NAMES_POSTERIORI = config.get('Basic Configuration', 'ANCESTRIES_POSTERIORI')
+ANCESTRY_NAMES_POSTERIORI = ANCESTRY_NAMES_POSTERIORI.split(',')
 SEXUAL_BIAS = int(config.get('Basic Configuration', 'SEXUAL_BIAS'))
 
 #filter config
@@ -29,17 +31,19 @@ temp_colours = config.get('Graph Configuration', 'ANCESTRIES_COLOURS')
 temp_colours = temp_colours.split(',')
 for col in temp_colours:
     ANCESTRIES_COLOURS.append(colors.to_rgba(col))
-
 SELECT_ANCESTRIES = config.get('Graph Configuration', 'SELECT_ANCESTRIES')
-# removing comma if added to last element
 if SELECT_ANCESTRIES[-1] == ',': SELECT_ANCESTRIES = SELECT_ANCESTRIES[:-1]
 SELECT_ANCESTRIES = SELECT_ANCESTRIES.split(',')
 
 # getting file path from command line
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', type=str, required=True)
+if GRAPH_TYPE == "priori_posteriori":
+    parser.add_argument('-p', '--posteriori', type=str, required=True)
 args = parser.parse_args()
 FILE_PATH = args.file
+if GRAPH_TYPE == "priori_posteriori":
+    POSTERIORI_PATH = args.posteriori
 
 math_tools = MathTools(SEXUAL_BIAS, NUMBER_PULSES, ANCESTRY_NAMES)
 plot_tools = PlotTools(SEXUAL_BIAS, NUMBER_PULSES, ANCESTRY_NAMES, ANCESTRIES_COLOURS)
@@ -58,12 +62,20 @@ for anc in ANCESTRY_NAMES:
     ancestries_string += ','
 ancestries_string = ancestries_string[:-1]
 
-input_reader = InputReader(FILE_PATH, NUMBER_PULSES, NUMBER_ANCESTRY, ancestries_string, SEXUAL_BIAS)
-
-df = input_reader.read_input()
-
-#applying HDR filter 
+input_reader = InputReader(NUMBER_PULSES, NUMBER_ANCESTRY, ancestries_string, SEXUAL_BIAS)    
+df = input_reader.read_input(FILE_PATH)
 if(FILTER == 1): df = math_tools.filter(HDR, df)
+
+if GRAPH_TYPE == "priori_posteriori":
+    ancestries_string_posteriori = str()
+    for anc in ANCESTRY_NAMES_POSTERIORI:
+        ancestries_string_posteriori += anc
+        ancestries_string_posteriori += ','
+    ancestries_string_posteriori = ancestries_string_posteriori[:-1]
+    input_reader_posteriori = InputReader(NUMBER_PULSES, NUMBER_ANCESTRY, ancestries_string_posteriori, SEXUAL_BIAS)
+    df_posteriori = input_reader_posteriori.read_input(POSTERIORI_PATH)
+    if(FILTER == 1):
+        df_posteriori = math_tools.filter(HDR, df_posteriori)
 
 match GRAPH_TYPE:
     case 'bars':
@@ -71,11 +83,16 @@ match GRAPH_TYPE:
     case 'lines':
         plot_tools.plot_lines(df, SELECT_ANCESTRIES, "./Outputs/Graphs/line_graph.png")
     case 'point-pulse':
-        plot_tools.plot_points__with_errorbars(df, SELECT_ANCESTRIES, "./Outputs/Graphs/point_graph_pulse.png")
+        plot_tools.plot_points_with_errorbars(df, SELECT_ANCESTRIES, "./Outputs/Graphs/point_graph_pulse.png")
     case 'point-ancestry':
-        plot_tools.plot_points__by_ancestry(df, SELECT_ANCESTRIES, "./Outputs/Graphs/point_graph_ancestry.png")
+        plot_tools.plot_points_by_ancestry(df, SELECT_ANCESTRIES, "./Outputs/Graphs/point_graph_ancestry.png")
+    case 'priori_posteriori':
+        plot_tools.priori_posteriori(df, df_posteriori, SELECT_ANCESTRIES, "./Outputs/Graphs/priori_posteriori.png", True, percentage=HDR)
     case _:
         print("[ERROR] Error in graph type selection. Check config.ini file.")
         exit()
 
-math_tools.write_stats(df, "./Outputs/Statistics/stats.csv")
+if GRAPH_TYPE == "priori_posteriori":
+    math_tools.write_stats(df, "./Outputs/Statistics/stats_priori.csv")
+    math_tools.write_stats(df_posteriori, "./Outputs/Statistics/stats_posteriori.csv")
+else: math_tools.write_stats(df, "./Outputs/Statistics/stats.csv")
--- tools/plot_tools.py
+++ tools/plot_tools.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
+import numpy as np
 import seaborn as sns
+import pandas as pd
 
 from tools.math_tools import MathTools
 
@@ -11,6 +13,17 @@ class PlotTools:
         self.ancestry_names = ancestry_names
         self.ancestry_colours = ancestry_colours
         self.math_tools = MathTools(sexual_bias, n_pulses, ancestry_names)
+
+    def plot_HPD_lines(self, df, axis, percentage, color):
+        lower_quantile = np.percentile(df["Value"], ((100-percentage)/2), method="closest_observation")
+        upper_quantile = np.percentile(df["Value"], (100-((100-percentage)/2)), method="closest_observation")
+
+        #quantile bounds lines
+        axis.axvline(x=lower_quantile, color=color, linestyle='--', linewidth=0.8)
+        axis.axvline(x=upper_quantile, color=color, linestyle='--', linewidth=0.8)
+        
+        #mode line
+        axis.axvline(x=self.math_tools.find_mode(df), color="black", linestyle=':', linewidth=1.2)
         
     #plots histograms for each pulse, separated by sex
     #ancestries param should be a list with the ancestries' names, eg. ancestries = ["EUR", "AFR", "NAT", "HYB"]
@@ -26,15 +39,12 @@ class PlotTools:
         df_hist = df[(df["Ancestry"].isin(selected_ancestries)) & (df["Value"] != 0)]
 
         if self.sexual_bias: graph = sns.FacetGrid(df_hist, col="Sex", row="Pulse", hue="Ancestry", palette=colours)
-        else: graph = sns.FacetGrid(df_hist, col="Sex", row="Pulse", hue="Ancestry", palette=colours)
+        else: graph = sns.FacetGrid(df_hist, col="Pulse", hue="Ancestry", palette=colours)
 
         graph = (graph.map_dataframe(sns.histplot, x="Value", multiple=type, stat='probability').add_legend())
-        plt.xlim([0, 1])
+        plt.xlim([-0.2, 1])
         graph.savefig(savefile)
 
-    #plots the line graph and saves
-    #ancestries param should be a list with the ancestries' names, eg. ancestries = ["EUR", "AFR", "NAT", "HYB"]
-    #if lines=True, the graph will show mode and quantiles for the percentage
     def plot_lines(self, df, selected_ancestries, savefile):
         colours = list()
 
@@ -46,12 +56,12 @@ class PlotTools:
 
         df_hist = df[(df["Ancestry"].isin(selected_ancestries)) & (df["Value"] != 0)]
 
-        if self.sexual_bias: graph = sns.FacetGrid(df_hist, col="Sex", row="Pulse", hue="Ancestry", palette=colours)
-        else: graph = sns.FacetGrid(df_hist, col="Sex", row="Pulse", hue="Ancestry", palette=colours)
+        if self.sexual_bias: graph = sns.FacetGrid(df_hist, col="Pulse", hue="Ancestry", palette=colours)
+        else: graph = sns.FacetGrid(df_hist, col= "Pulse", hue="Ancestry", palette=colours)
 
         graph = (graph.map_dataframe(sns.histplot, x="Value", fill=False, linewidth=0, kde=True, stat='probability'))
 
-        plt.xlim([0, 1])
+        plt.xlim([-0.2, 1])
         
         #drawing the legend for each ancestry
         legend = list()
@@ -63,7 +73,7 @@ class PlotTools:
         
         graph.savefig(savefile)
 
-    def plot_points__by_ancestry(self, df, selected_ancestries, savefile):
+    def plot_points_by_ancestry(self, df, selected_ancestries, savefile):
         colours = list()
 
         for anc1 in range(0, len(selected_ancestries)):
@@ -137,7 +147,7 @@ class PlotTools:
                     axis[anc].plot([point_x, point_x], [min_temp, max_temp], linewidth=1, linestyle='--', color=colours[anc], alpha=0.9)
 
             #setting axis limits, ticks and title
-            axis[anc].set_xlim(0, 1)
+            axis[anc].set_xlim(-0.2, 1)
             axis[anc].set_ylim(0, 1)
             axis[anc].set_xticks(ticks, labels)
             axis[anc].set_title(selected_ancestries[anc])
@@ -147,8 +157,7 @@ class PlotTools:
         figure.savefig(savefile)
 
 
-    def plot_points__with_errorbars(self, df, selected_ancestries, savefile):
-        sexes = ("Female", "Male")
+    def plot_points_with_errorbars(self, df, selected_ancestries, savefile):
         colours = list()
 
         for anc1 in range(0, len(selected_ancestries)):
@@ -166,13 +175,14 @@ class PlotTools:
         labels = list()
         
         for anc in range(0,len(selected_ancestries)):
-            ticks.append((1/(len(selected_ancestries)+1))*(anc+1))
+            ticks.append((1/(len(selected_ancestries)+2))*(anc+1))
             labels.append(selected_ancestries[anc])
 
         df_plot = df
         for pulse in range(1,(self.n_pulses+1)):
             for anc in range(0,len(selected_ancestries)):
                 if self.sexual_bias:
+                    sexes = ("Female", "Male")
                     for sex in range(0,2):
                         #selecting only the values from this pulse, sex and ancestry
                         df_temp = df_plot[(df_plot["Pulse"] == pulse) & (df_plot["Sex"] == sexes[sex]) & (df_plot["Ancestry"] == selected_ancestries[anc])]
@@ -209,7 +219,7 @@ class PlotTools:
                     mean_temp = df_temp["Value"].mean()
 
                     #plotting point
-                    point_x = ((1/(len(selected_ancestries)+1))*(anc+1))
+                    point_x = ((1/(len(selected_ancestries)+2))*(anc+1))
                     axis[pulse-1].plot(point_x, mean_temp, marker='o', markersize=6, color=colours[anc])
 
                     #plotting min and max lines
@@ -221,7 +231,7 @@ class PlotTools:
                     axis[pulse-1].plot([point_x, point_x], [min_temp, max_temp], linewidth=1, linestyle='--', color=colours[anc], alpha=0.9)
 
             #setting axis limits, ticks and title
-            axis[pulse-1].set_xlim(0, 1)
+            axis[pulse-1].set_xlim(-0.2, 1)
             axis[pulse-1].set_ylim(0, 1)
             axis[pulse-1].set_xticks(ticks, labels)
             axis[pulse-1].set_title("Pulse {}".format(pulse))
@@ -235,5 +245,39 @@ class PlotTools:
         figure.subplots_adjust(right=0.925)
         axis[0].set_ylabel("Ancestry Percentage")
         
+        figure.savefig(savefile)
+
+    def priori_posteriori(self, df_priori, df_posteriori, selected_ancestries, savefile):
+        colours = list()
+
+        for anc1 in range(0, len(selected_ancestries)):
+            for anc2 in range(0, len(self.ancestry_names)):
+                if selected_ancestries[anc1] == self.ancestry_names[anc2]:
+                    colours.append(self.ancestry_colours[anc2])
+                    break
         
+        df_priori_filtered = df_priori[(df_priori["Ancestry"].isin(selected_ancestries)) & (df_priori["Value"] != 0)]
+        df_posteriori_filtered = df_posteriori[(df_posteriori["Ancestry"].isin(selected_ancestries)) & (df_posteriori["Value"] != 0)]
+
+        figure, axis = plt.subplots(self.n_pulses, len(selected_ancestries))
+
+        figure.set_figheight(8)
+        figure.set_figwidth(9)
+        for anc in range(0, len(selected_ancestries)):
+            for pulse in range(0, self.n_pulses):
+                axis[pulse][anc].set_title(f"{selected_ancestries[anc]} Pulse {pulse+1}")
+                df_priori_temp = df_priori_filtered[(df_priori_filtered["Pulse"] == pulse+1) & (df_priori_filtered["Ancestry"] == selected_ancestries[anc])]
+                df_posteriori_temp = df_posteriori_filtered[(df_posteriori_filtered["Pulse"] == pulse+1) & (df_posteriori_filtered["Ancestry"] == selected_ancestries[anc])]
+                sns.kdeplot(x="Value", data=df_priori_temp, ax=axis[pulse][anc], color=colours[anc], linestyle="--")
+                sns.kdeplot(x="Value", data=df_posteriori_temp, ax=axis[pulse][anc], color=colours[anc], linestyle="-")
+
+                axis[pulse][anc].set_xlim(-0.2, 1)
+
+        legend = list()
+        legend.append(mlines.Line2D([], [], color="black", ls='--', label="Priori"))
+        legend.append(mlines.Line2D([], [], color="black", ls='-', label="Posteriori"))
+        figure.legend(handles=legend, loc=7, frameon=False)
+
+        figure.tight_layout(pad=1)
+        figure.subplots_adjust(right=0.85)
         figure.savefig(savefile)
